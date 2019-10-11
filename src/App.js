a2b11192b98d77264bc55ee99610cd3c1201e8b3
marvin omar tola pacheco <mtolapacheco@gmail.com>
2019-10-11 08:42:09 -0400

    integrando App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./styles/styles.scss"
 
-const App=() => <h1>Hola mundo</h1>
+const App=() => <h1>Hola mundo desde mi App.js con EDteam</h1>
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
 const root = document.getElementById("root")
 
-ReactDOM.render(<h1 className="saludos">Hola mundo</h1>,root);
+ReactDOM.render(<App />,root);
