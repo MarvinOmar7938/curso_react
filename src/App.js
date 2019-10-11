5624f317c0424339014473526fed9cd1485f9f6b
marvin omar tola pacheco <mtolapacheco@gmail.com>
2019-10-11 09:37:49 -0400

    JSX a profundidad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import "./styles/styles.scss"
+import Curso from './Curso'
 
 const App=() => (
   <>
-    <div className="saludo">
-      <h1>Hola mundo</h1>
-    <p  >hola, saludando desde mi componente</p>
+    <div className="main-banner img-container l-section" id="main-banner">
+    <div className="ed-grid lg-grid-6">
+      <div className="lg-cols-4 lg-x-2">
+        <img className="main-banner__img" 
+              src="https://wallpapercave.com/wp/wp3102477.jpg" />
+        <div className="main-banner__data s-center">
+          <p className="t2 s-mb-0">Cursos de EDteam</p>
+          <p> Tu futuro te esta esperando</p>
+          <a href="https://ed.team" className="button">Suscribirse</a>
+        </div>
     </div>
-    <div>
-      <img src="https://cdn-www.bluestacks.com/bs-images/Saint-Seiya-Awakening_Summoning-Best-Characters_ES_2.jpg"/>
-    </div>
+  </div>
+</div>
+<div className="ed-grid m-grid-3">
+  <Curso />
+  <Curso />
+  <Curso />
+  <Curso />
+  <Curso />
+  <Curso />
+  <Curso />
+  <Curso />
+</div>
   </>
 )
 
 export default App;
-
-//reglas JSX
-//1: toda etiqueta debe cerrarse
-//2: Los componentes deben devolver un solo elemento padre
-//3: Apoyarse de los fragments cuando necesito devolver 2 elementos
-//4: Fragment => <>hijos</>
-//5: img siempre se cierra
-//6: class =>className
-//7: for => htmlFor
