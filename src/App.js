8aea879689de6aac2be5916a47b0ba4ac5a452c4
marvin omar tola pacheco <mtolapacheco@gmail.com>
2019-10-11 10:56:46 -0400

    expresiones JSX

--- src/App.js
+++ src/App.js
@@ -19,13 +19,6 @@ const App=() => (
 </div>
 <div className="ed-grid m-grid-3">
   <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
 </div>
   </>
 )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
+const mayorEdad = edad => edad>18
+
+const persona = {"nombre":"Marvin" , "apellido":"Tola","edad":35}
+
 const Curso=() => (
     <article className="card">
     <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
       <img src="https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-08/angular.png" alt="poster del curso" />
     </div>
     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-      <h3 className="t5 s-mb-2 s-center">
-        Programación orientada a objetos con Go
-      </h3>
+        {
+           mayorEdad(persona.edad) ? <h3>Soy mayor</h3>:<p>soy menor</p>
+        }
       <div className="s-mb-2 s-main-center">
         <div className="card__teacher s-cross-center">
           <div className="card__avatar s-mr-1">
@@ -16,7 +20,7 @@ const Curso=() => (
               <img src="https://api.ed.team/files/avatars/66813820-2857-4af9-b84f-9196acbb832e.jpg" alt="tio tola" />
             </div>
           </div>
-          <span className="small">Alexys Lozada</span>
+          <span className="small">{`${persona.nombre} ${persona.apellido}`}</span>
         </div>
       </div>
       <div className="s-main-center">
